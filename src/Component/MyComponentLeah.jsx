02085a5790aac5a0357b6e3886e908392febcf3c
הנדסת תוכנה <huser@sem.local>
2025-12-24 20:21:28 +0200

    leah

--- src/Component/MyComponentLeah.jsx
+++ src/Component/MyComponentLeah.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const MyComponent = () => {
     return (
         <div>
-            <h1>שלום מקומפוננטת MyComponent!</h1>
-            <p>זוהי דוגמה לקומפוננטה חדשה ב-React.</p>
+            <h1>  שלום</h1>
+            
         </div>
     );
 };
