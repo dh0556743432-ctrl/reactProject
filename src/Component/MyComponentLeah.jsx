519e72d961addfe6f9d7f2d430dc3b459f7c655b
הנדסת תוכנה <huser@sem.local>
2025-12-24 19:54:14 +0200

    בדיקה

---
+++ src/Component/MyComponentLeah.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MyComponent = () => {
+    return (
+        <div>
+            <h1>שלום מקומפוננטת MyComponent!</h1>
+            <p>זוהי דוגמה לקומפוננטה חדשה ב-React.</p>
+        </div>
+    );
+};
+
+export default MyComponent;
