42b17e5fce7324d854a4f473417299c729ea27d0
Naama00 <n0533110130@gmail.com>
2025-12-25 21:51:30 +0200

    Add new page to the project

---
+++ src/Component/Naama'sComponent.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const NaamasComponent = () => {
+    return (
+        <div>
+            <h1>it's really fun that I did it finally!</h1>
+        </div>
+    );
+};
+export default NaamasComponent;
